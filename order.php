ef5b9031d30f10252d451eedeff88980063e3884
Exequel <exql.adzn@gmail.com>
2023-05-22 09:31:31 +0800

    connect order form to php (initial)

--- order.php
+++ order.php
@@ -4,17 +4,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Order Summary</title>
 </head>
 <body>
-    <?php
-        $name = $_POST['name'];
-        $order = $_POST['order'];
-        $address = $_POST['address'];
-        $message = $_POST['message'];
+<?php
+// Check if form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Retrieve form data
+  $customerName = $_POST['customer-name'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  $category = $_POST['category'];
+  $items = $_POST['items'];
+  $additionalNotes = $_POST['additional-notes'];
 
-        echo "<h1>Order Received! <br></h1>";
-        echo "$name <br> $order <br> $address <br> $message"; 
-    ?>
+  // Display submitted order details
+  echo "<h2>Order Details:</h2>";
+  echo "<p><strong>Customer Name:</strong> $customerName</p>";
+  echo "<p><strong>Phone Number:</strong> $phone</p>";
+  echo "<p><strong>Email Address:</strong> $email</p>";
+  echo "<p><strong>Category:</strong> $category</p>";
+  echo "<p><strong>Items:</strong></p>";
+  echo "<ul>";
+  foreach ($items as $item) {
+    echo "<li>$item</li>";
+  }
+  echo "</ul>";
+  echo "<p><strong>Additional Notes:</strong> $additionalNotes</p>";
+}
+?>
+
 </body>
 </html>
