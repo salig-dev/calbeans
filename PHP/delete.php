4dcea94b7441151d05a00be3a451a21f9e54cfd8
Clement Cabus <chummycabus@gmail.com>
2023-05-22 08:05:19 +0800

    DASHBOARD PROTOTYPE 1

---
+++ PHP/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_GET["id"])){
+    $id = $_GET["id"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "myshop";
+
+//create connection
+$conn = new mysqli($servername,$username,$password,$database);
+
+    $sql = "DELETE FROM clients WHERE id=$id";
+    $conn->query($sql);
+}
+
+header("location: /calbeans php/index.php");
+exit;
+?>
