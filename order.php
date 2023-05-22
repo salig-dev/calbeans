0291660be327dfb67edf8fe59dfc3f79380373dc
Exequel <exql.adzn@gmail.com>
2023-05-22 14:47:12 +0800

    update order forms  - test saving functionalities  - still needs fixing  - no quantity + prices

--- order.php
+++ order.php
@@ -14,25 +14,25 @@ $phone = $_POST['phone'];
 $email = $_POST['email'];
 $category = $_POST['category'];
 $items = $_POST['items'];
-$quantities = $_POST['quantity'];
+// $quantities = $_POST['quantity'];
 $additionalNotes = $_POST['additional-notes'];
 
 // Fixed prices for each item
-$itemPrices = array(
-  "Americano (8 oz)" => 69.00,
-  "Vanilla Hot (12 oz)" => 69.00,
-  "Vanilla Cold Brew (16 oz)" => 100.00,
-  // Add more items and prices here for other categories
-);
+// $itemPrices = array(
+//   "Americano (8 oz)" => 69.00,
+//   "Vanilla Hot (12 oz)" => 69.00,
+//   "Vanilla Cold Brew (16 oz)" => 100.00,
+//   // Add more items and prices here for other categories
+// );
 
 // Calculate the total price
-$totalPrice = 0;
-for ($i = 0; $i < count($items); $i++) {
-  $item = $items[$i];
-  $quantity = $quantities[$i];
-  $price = $itemPrices[$item];
-  $totalPrice += $price * $quantity;
-}
+// $totalPrice = 0;
+// for ($i = 0; $i < count($items); $i++) {
+//   $item = $items[$i];
+//   $quantity = $quantities[$i];
+//   $price = $itemPrices[$item];
+//   $totalPrice += $price * $quantity;
+// }
 
 // Display the order summary
 echo "Order Summary:<br>";
@@ -43,9 +43,10 @@ echo "Category: " . $category . "<br>";
 echo "Items:<br>";
 for ($i = 0; $i < count($items); $i++) {
   $item = $items[$i];
-  $quantity = $quantities[$i];
-  $price = $itemPrices[$item];
-  echo $item . " - Quantity: " . $quantity . " - Price: $" . $price . "<br>";
+//   $quantity = $quantities[$i];
+//   $price = $itemPrices[$item];
+  echo $item . "<br>";
+//   echo $item . " - Quantity: " . $quantity . " - Price: $" . $price . "<br>";
 }
 echo "Total Price: $" . $totalPrice . "<br>";
 echo "Additional Notes: " . $additionalNotes;
