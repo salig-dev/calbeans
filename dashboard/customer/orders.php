8d7fbb92320696c40acc9a9806aa73a173d8a3ac
kel <michaeljosh.bargabino@gmail.com>
2023-05-29 18:42:48 +0800

    peso sign included

--- dashboard/customer/orders.php
+++ dashboard/customer/orders.php
@@ -67,7 +67,7 @@ require_once('partials/_head.php');
                       </td>
                       <td><?php echo $prod->prod_code; ?></td>
                       <td><?php echo $prod->prod_name; ?></td>
-                      <td>$ <?php echo $prod->prod_price; ?></td>
+                      <td>₱ <?php echo $prod->prod_price; ?></td>
                       <td>
                         <a href="make_oder.php?prod_id=<?php echo $prod->prod_id; ?>&prod_name=<?php echo $prod->prod_name; ?>&prod_price=<?php echo $prod->prod_price; ?>">
                           <button class="btn btn-sm btn-warning">
