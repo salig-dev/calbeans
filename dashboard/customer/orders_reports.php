cd5e0a4db645fa3f104db8f809bae797baad055e
Mark Angelo <markangelosiazon@gmail.com>
2023-05-29 21:08:30 +0800

    Minor adjust
    - Peso Sign

--- dashboard/customer/orders_reports.php
+++ dashboard/customer/orders_reports.php
@@ -63,9 +63,9 @@ require_once('partials/_head.php');
                                             <th class="text-success" scope="row"><?php echo $order->order_code; ?></th>
                                             <td><?php echo $order->customer_name; ?></td>
                                             <td class="text-success"><?php echo $order->prod_name; ?></td>
-                                            <td>$ <?php echo $order->prod_price; ?></td>
+                                            <td>₱ <?php echo $order->prod_price; ?></td>
                                             <td class="text-success"><?php echo $order->prod_qty; ?></td>
-                                            <td>$ <?php echo $total; ?></td>
+                                            <td>₱ <?php echo $total; ?></td>
                                             <td><?php if ($order->order_status == '') {
                                                     echo "<span class='badge badge-danger'>Not Paid</span>";
                                                 } else {
