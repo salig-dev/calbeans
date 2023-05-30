f79006ad8edc7d0eb6505523791b3c036ea360e6
Mark Angelo <markangelosiazon@gmail.com>
2023-05-31 01:34:44 +0800

    Added Orders Function (Similar to customer)

--- dashboard/admin/orders.php
+++ dashboard/admin/orders.php
@@ -7,15 +7,9 @@ check_login();
 require_once('partials/_head.php');
 ?>
 
-<style>
+<link rel="stylesheet" href="../../assets/css/nice-select.css" />
+<link rel="stylesheet" href="../../assets/css/dashboard.css">
 
-.__prod_name {
-  overflow-wrap: break-word;
-  white-space: break-spaces !important;
-  max-width: 140px;
-}
-
-</style>
 <body>
   <!-- Sidenav -->
   <?php
@@ -29,25 +23,26 @@ require_once('partials/_head.php');
     ?>
     <!-- Header -->
     <div style="background-image: url(assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
-    <span class="mask bg-gradient-dark opacity-8"></span>
+      <span class="mask bg-gradient-dark opacity-8"></span>
       <div class="container-fluid">
         <div class="header-body">
         </div>
       </div>
     </div>
     <!-- Page content -->
-    <div class="container-fluid mt--8">
+    <div class="container-fluid mt--8 overflow-visible">
       <!-- Table -->
-      <div class="row">
-        <div class="col">
-          <div class="card shadow">
-                                    <!-- SELECTION MENU-->
-                                    <section class="row mx-0 py-2">
-              <div class="card-header border-0 col-lg-5 col-md-6 col-11 text-md-left text-center mx-auto">Select On Any Product To Make An Order</div>
+        <div class="row">
+          <div class="col mx-auto card shadow">
+            <!-- SELECTION MENU-->
+            <section class="row mx-0 py-2">
+              <div class="card-header border-0 col-xl-6 col-lg-7 col-md-6 col-11 text-md-left text-center mx-auto">
+                Select On Any Product To Make An Order
+              </div>
 
-              <div class="__menu-select-ctn col-lg-7 col-md-6 col-10  mx-auto d-flex justify-content-start align-items-center">
-                <select class="__menu-select-sub-ctn text-center w-75 mx-auto" id="menu-combobox" onchange="main();">
-                  <option disabled value="disabled">Select a category</option>
+              <div class="__menu-select-ctn col-xl-6 col-lg-5 col-md-6 col-10  mx-auto d-flex justify-content-start align-items-center">
+                <select placeholder="Select a category" class="__menu-select-sub-ctn text-center w-75 mx-auto" id="menu-combobox" onchange="main();">
+                  <option disabled selected value="disabled">Select a category</option>
                   <option value="All">All</option>
                   <option value="Espresso">Espresso</option>
                   <option value="Fresh Black Coffee/Cold Brew">Fresh Black Coffee/Cold Brew</option>
@@ -62,17 +57,19 @@ require_once('partials/_head.php');
               </div>
 
             </section>
-            <div class="table-responsive">
+
+            <div class="table-responsive mt-lg-2 mt-md-4 mt-5 ">
               <table class="table align-items-center table-flush">
                 <thead class="thead-light">
                   <tr>
-                    <th scope="col"><b>Image</b></th>
-                    <th scope="col"><b>Product Code</b></th>
-                    <th scope="col"><b>Name</b></th>
-                    <th scope="col"><b>Price</b></th>
-                    <th scope="col"><b>Action</b></th>
+                    <th class="__prod_code __col-odd"scope="col">Product Code</th>
+                    <th class="__prod_name" scope="col">Name</th>
+                    <th class="__prod_category __col-odd" scope="col">Category</th>
+                    <th class="__prod_price" scope="col">Price</th>
+                    <th class="__col-odd" scope="col">Action</th>
                   </tr>
                 </thead>
+
                 <tbody>
                   <?php
                   $ret = "SELECT * FROM  rpos_products ";
@@ -82,21 +79,14 @@ require_once('partials/_head.php');
                   while ($prod = $res->fetch_object()) {
                   ?>
                     <tr>
-                      <td>
-                        <?php
-                        if ($prod->prod_img) {
-                          echo "<img src='assets/img/products/$prod->prod_img' height='60' width='60 class='img-thumbnail'>";
-                        } else {
-                          echo "<img src='assets/img/products/default.jpg' height='60' width='60 class='img-thumbnail'>";
-                        }
-
-                        ?>
-                      </td>
-                      <td><?php echo $prod->prod_code; ?></td>
-                      <td class = "__prod_name"><?php echo $prod->prod_name; ?></td>
-                      <td>₱ <?php echo $prod->prod_price; ?></td>
-                      <td>
-                        <a href="make_oder.php?prod_id=<?php echo $prod->prod_id; ?>&prod_name=<?php echo $prod->prod_name; ?>&prod_price=<?php echo $prod->prod_price; ?>">
+                      <td class="__prod_code __col-odd"><?php echo $prod->prod_code; ?></td>
+                      <td class="__prod_name"><?php echo $prod->prod_name; ?></td>
+                      <td class="__prod_category __col-odd"><?php echo $prod->prod_category; ?></td>
+                      <td class="__prod_price">₱ <?php echo $prod->prod_price; ?></td>
+                      <td class="__col-odd"> <!-- Place Order Button : Admin -->
+                        <a href="make_oder.php?prod_id=<?php echo $prod->prod_id; ?>
+                        &prod_name=<?php echo $prod->prod_name; ?>
+                        &prod_price=<?php echo $prod->prod_price; ?>">
                           <button class="btn btn-sm btn-warning">
                             <i class="fas fa-cart-plus"></i>
                             Place Order
@@ -110,7 +100,6 @@ require_once('partials/_head.php');
             </div>
           </div>
         </div>
-      </div>
       <!-- Footer -->
       <?php
       require_once('partials/_footer.php');
@@ -122,16 +111,17 @@ require_once('partials/_head.php');
   require_once('partials/_scripts.php');
   ?>
 
-<script src="../../assets/js/vendor/jquery-1.12.4.min.js"></script>
-<script src="../../assets/js/jquery.nice-select.min.js"></script>
+  <script src="../../assets/js/vendor/jquery-1.12.4.min.js"></script>
+  <script src="../../assets/js/jquery.nice-select.min.js"></script>
 
-<script>
-  $(document).ready(function() {
-    $('select').niceSelect();
-  });
-</script>
+  <script>
+    $(document).ready(function() {
+      $('select').niceSelect();
+    });
+  </script>
 
-<script src="js/orders.js"></script>
+  <script src="js/orders.js"></script>
 
 </body>
+
 </html>
