2bb00313919abd2d9d07fbd2972bdbe212842530
Clement Cabus <chummycabus@gmail.com>
2023-05-31 14:10:13 +0800

    order summary fix -single update fix

--- dashboard/admin/update_order_status.php
+++ dashboard/admin/update_order_status.php
@@ -8,33 +8,18 @@ if (isset($_POST['update_status'])) {
     $order_id = $_POST['order_id'];
     $new_status = $_POST['new_status'];
 
-    // Fetch the order details including customer name
-    $stmt = $mysqli->prepare("SELECT * FROM rpos_orders WHERE order_id = ?");
-    $stmt->bind_param("i", $order_id);
+    // Update the order status
+    $stmt = $mysqli->prepare("UPDATE rpos_orders SET order_status = ? WHERE order_id = ?");
+    $stmt->bind_param("si", $new_status, $order_id);
     $stmt->execute();
-    $result = $stmt->get_result();
-    $order = $result->fetch_object();
 
-    if ($order) {
-        // Update the order status in the database for orders with the same customer name
-        $stmt = $mysqli->prepare("UPDATE rpos_orders SET order_status = ? WHERE customer_name = ?");
-        $stmt->bind_param("ss", $new_status, $order->customer_name);
-        $stmt->execute();
-
-        if ($stmt->affected_rows > 0) {
-            $_SESSION['success'] = "Order status updated successfully";
-        } else {
-            $_SESSION['error'] = "Failed to update order status";
-        }
-
-        $stmt->close();
+    if ($stmt->affected_rows > 0) {
+        $_SESSION['success'] = "Order status updated successfully";
     } else {
-        $_SESSION['error'] = "Order not found";
+        $_SESSION['error'] = "Failed to update order status";
     }
 
-    $mysqli->close();
-
-    header("Location: orders_reports.php");
+    header("Location: orders_reports.php"); // Redirect back to the order list page
     exit();
 }
 ?>
