85ca00a361e474d608442020cf0029813797099f
Mark Angelo <markangelosiazon@gmail.com>
2023-06-04 00:13:58 +0800

    Update backend footer

--- dashboard/customer/partials/_footer.php
+++ dashboard/customer/partials/_footer.php
@@ -3,13 +3,13 @@
     <div class="row align-items-center justify-content-xl-between">
       <div class="col-xl-6">
         <div class="copyright text-center text-xl-left text-muted">
-        &copy; 2020 - <?php echo date('Y'); ?> - Developed By Martin Mbithi Nzilani
+        &copy; 2020 - <?php echo date('Y'); ?> - Developed By Salig Tech
         </div>
       </div>
       <div class="col-xl-6">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
           <li class="nav-item">
-            <a href="" class="nav-link" target="_blank">Restaurant POS</a>
+            <a href="https://www.facebook.com/calbeanscoffee" class="nav-link" target="_blank">Calbeans</a>
           </li>
         </ul>
       </div>
