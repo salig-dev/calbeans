a27c458e73dd188934eeeb934922ff21124ea4fd
Clement Cabus <chummycabus@gmail.com>
2023-06-05 15:01:02 +0800

    Sales Update -prototype #4 -maybe need some changes but works lololol

--- dashboard/admin/sales.php
+++ dashboard/admin/sales.php
@@ -16,7 +16,6 @@ if ($result->num_rows > 0) {
     $topProducts = array(); // Set an empty array if no top products found
 }
 
-
 // Fetch Daily Sales Data
 $dailySalesData = array();
 $result = $mysqli->query("SELECT DATE(created_at) AS order_date, SUM(prod_price * prod_qty) AS total_sales FROM rpos_orders GROUP BY DATE(created_at)");
@@ -24,7 +23,13 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $dailySalesData[$row['order_date']] = $row['total_sales'];
     }
+} else {
+    $dailySalesData[date('Y-m-d')] = 0; // Set today's sales to 0 if no sales found
 }
+
+// Get today's sales amount
+$todaySales = isset($dailySalesData[date('Y-m-d')]) ? $dailySalesData[date('Y-m-d')] : 0;
+
 
 // Fetch Monthly Sales Data
 $monthlySalesData = array();
@@ -33,6 +38,14 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $monthlySalesData[$row['order_month']] = $row['total_sales'];
     }
+}
+
+// Calculate Yearly Sales
+$yearlySales = 0;
+$result = $mysqli->query("SELECT SUM(prod_price * prod_qty) AS total_sales FROM rpos_orders");
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $yearlySales = $row['total_sales'];
 }
 ?>
 
@@ -75,62 +88,97 @@ if ($result->num_rows > 0) {
 
         <!-- Page content -->
         <div class="container-fluid mt--8">
-        <div class="row">
-            <div class="col-lg-12">
-                <!-- Top 10 Products -->
+            <div class="row">
+                <div class="col-lg-4">
+                   <!-- Daily Sales Summary -->
                 <div class="card shadow mb-4">
-                    <div class="card-header border-0">
-                        <h3 class="mb-0">Top 10 Products</h3>
+                    <div class="card-body">
+                        <h5 class="card-title">Daily Sales</h5>
+                        <h3 class="card-text">₱<?php echo $todaySales; ?></h3>
                     </div>
-                    <div class="table-responsive" style="padding: 0 20px;">
-                        <table class="table align-items-center table-flush">
-                            <thead class="thead-light">
-                                <tr>
-                                    <th>Product</th>
-                                    <th class="text-right">Quantity Sold</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($topProducts as $product => $quantity) {
-                                    echo "<tr>";
-                                    echo "<td>$product</td>";
-                                    echo "<td class='text-right'>$quantity</td>";
-                                    echo "</tr>";
-                                }
-                                ?>
-                            </tbody>
-                        </table>
+                </div>
+                </div>
+                <div class="col-lg-4">
+                    <!-- Monthly Sales Summary -->
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <h5 class="card-title">Monthly Sales</h5>
+                            <h3 class="card-text">₱<?php echo array_sum($monthlySalesData); ?></h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <!-- Yearly Sales Summary -->
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <h5 class="card-title">Yearly Sales</h5>
+                            <h3 class="card-text">₱<?php echo $yearlySales; ?></h3>
+                        </div>
                     </div>
                 </div>
             </div>
+            <div class="row">
+            <div class="col-lg-6">
+    <!-- Top 10 Products -->
+    <div class="card shadow mb-4">
+        <div class="card-header border-0">
+            <h3 class="mb-0">Top 10 Products</h3>
         </div>
+        <div class="table-responsive" style="padding: 0 20px;">
+            <?php if (count($topProducts) > 0) { ?>
+                <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>Rank</th>
+                            <th>Product</th>
+                            <th class="text-right">Quantity Sold</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $rank = 1;
+                        foreach ($topProducts as $product => $quantity) {
+                            echo "<tr>";
+                            echo "<td>$rank</td>";
+                            echo "<td>$product</td>";
+                            echo "<td class='text-right'>$quantity</td>";
+                            echo "</tr>";
+                            $rank++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php } else { ?>
+                <p>No top products found.</p>
+            <?php } ?>
+        </div>
+    </div>
+</div>
 
-        <div class="row">
-            <div class="col-lg-6">
-                <!-- Daily Sales Chart -->
-                <div class="card shadow mb-4">
-                    <div class="card-header border-0">
-                        <h3 class="mb-0">Daily Sales</h3>
+                <div class="col-lg-6">
+                    <!-- Daily Sales Chart -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header border-0">
+                            <h3 class="mb-0">Daily Sales</h3>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="dailySalesChart" style="height: 400px; width: 100%;"></canvas>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <canvas id="dailySalesChart" style="height: 400px; width: 100%;"></canvas>
+                </div>
+                <div class="col-lg-6">
+                    <!-- Monthly Sales Chart -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header border-0">
+                            <h3 class="mb-0">Monthly Sales</h3>
+                        </div>
+                        <div class="card-body">
+                            <canvas id="monthlySalesChart" style="height: 400px; width: 100%;"></canvas>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6">
-                <!-- Monthly Sales Chart -->
-                <div class="card shadow mb-4">
-                    <div class="card-header border-0">
-                        <h3 class="mb-0">Monthly Sales</h3>
-                    </div>
-                    <div class="card-body">
-                        <canvas id="monthlySalesChart" style="height: 400px; width: 100%;"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
+
             <!-- Footer -->
             <?php require_once('partials/_footer.php'); ?>
         </div>
@@ -140,141 +188,144 @@ if ($result->num_rows > 0) {
     <?php require_once('partials/_scripts.php'); ?>
 
    <!-- Initialize Daily Sales Chart -->
-<script>
-    var dailySalesData = <?php echo json_encode(array_values($dailySalesData)); ?>;
-    var dailySalesLabels = <?php echo json_encode(array_map(function($date) { return date('M j, Y', strtotime($date)); }, array_keys($dailySalesData))); ?>;
+    <script>
+        var dailySalesData = <?php echo json_encode(array_values($dailySalesData)); ?>;
+        var dailySalesLabels = <?php echo json_encode(array_map(function ($date) {
+            return date('M j, Y', strtotime($date));
+        }, array_keys($dailySalesData))); ?>;
 
-    var dailySalesChart = new Chart(document.getElementById('dailySalesChart'), {
-        type: 'line',
-        data: {
-            labels: dailySalesLabels,
-            datasets: [{
-                label: 'Daily Sales (in ₱)',
-                data: dailySalesData,
-                backgroundColor: 'rgba(78, 115, 223, 0.05)',
-                borderColor: 'rgba(78, 115, 223, 1)',
-                borderWidth: 2,
-                pointRadius: 5, // Increased the point radius to make dots bigger
-                pointBackgroundColor: 'rgba(78, 115, 223, 1)',
-                pointBorderColor: 'rgba(78, 115, 223, 1)',
-                pointHoverRadius: 5, // Increased the point hover radius to make dots bigger
-                pointHoverBackgroundColor: 'rgba(78, 115, 223, 1)',
-                pointHoverBorderColor: 'rgba(78, 115, 223, 1)',
-                pointHitRadius: 10,
-                pointBorderWidth: 2,
-                lineTension: 0.3
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: true,
-            scales: {
-                x: {
-                    type: 'time',
-                    time: {
-                        unit: 'day',
-                        displayFormats: {
-                            day: 'MMM D, Y'
+        var dailySalesChart = new Chart(document.getElementById('dailySalesChart'), {
+            type: 'line',
+            data: {
+                labels: dailySalesLabels,
+                datasets: [{
+                    label: 'Daily Sales (in ₱)',
+                    data: dailySalesData,
+                    backgroundColor: 'rgba(78, 115, 223, 0.05)',
+                    borderColor: 'rgba(78, 115, 223, 1)',
+                    borderWidth: 2,
+                    pointRadius: 5, // Increased the point radius to make dots bigger
+                    pointBackgroundColor: 'rgba(78, 115, 223, 1)',
+                    pointBorderColor: 'rgba(78, 115, 223, 1)',
+                    pointHoverRadius: 5, // Increased the point hover radius to make dots bigger
+                    pointHoverBackgroundColor: 'rgba(78, 115, 223, 1)',
+                    pointHoverBorderColor: 'rgba(78, 115, 223, 1)',
+                    pointHitRadius: 10,
+                    pointBorderWidth: 2,
+                    lineTension: 0.3
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                scales: {
+                    x: {
+                        type: 'time',
+                        time: {
+                            unit: 'day',
+                            displayFormats: {
+                                day: 'MMM D, Y'
+                            }
+                        },
+                        ticks: {
+                            source: 'auto',
+                            maxRotation: 0,
+                            autoSkip: true
                         }
                     },
-                    ticks: {
-                        source: 'auto',
-                        maxRotation: 0,
-                        autoSkip: true
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: function (value, index, values) {
+                                if (Math.floor(value) === value) {
+                                    return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                }
+                            }
+                        }
                     }
                 },
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        callback: function (value, index, values) {
-                            if (Math.floor(value) === value) {
-                                return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (context) {
+                                var value = context.parsed.y;
+                                if (Math.floor(value) === value) {
+                                    return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                } else {
+                                    return '₱' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                                }
                             }
                         }
                     }
+                },
+                legend: {
+                    display: false
                 }
-            },
-            plugins: {
-                tooltip: {
-                    callbacks: {
-                        label: function (context) {
-                            var value = context.parsed.y;
-                            if (Math.floor(value) === value) {
-                                return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                            } else {
-                                return '₱' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                            }
-                        }
-                    }
-                }
-            },
-            legend: {
-                display: false
             }
-        }
-    });
-</script>
+        });
+    </script>
 
-<!-- Initialize Monthly Sales Chart -->
-<script>
-    var monthlySalesData = <?php echo json_encode(array_values($monthlySalesData)); ?>;
-    var monthlySalesLabels = <?php echo json_encode(array_keys($monthlySalesData)); ?>;
+    <!-- Initialize Monthly Sales Chart -->
+    <script>
+        var monthlySalesData = <?php echo json_encode(array_values($monthlySalesData)); ?>;
+        var monthlySalesLabels = <?php echo json_encode(array_keys($monthlySalesData)); ?>;
 
-    var monthlySalesChart = new Chart(document.getElementById('monthlySalesChart'), {
-        type: 'bar',
-        data: {
-            labels: monthlySalesLabels,
-            datasets: [{
-                label: 'Monthly Sales (in ₱)',
-                data: monthlySalesData,
-                backgroundColor: 'rgba(78, 115, 223, 0.5)',
-                borderColor: 'rgba(78, 115, 223, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-            responsive: true,
-            scales: {
-                x: {
-                    grid: {
-                        display: false
+        var monthlySalesChart = new Chart(document.getElementById('monthlySalesChart'), {
+            type: 'bar',
+            data: {
+                labels: monthlySalesLabels,
+                datasets: [{
+                    label: 'Monthly Sales (in ₱)',
+                    data: monthlySalesData,
+                    backgroundColor: 'rgba(78, 115, 223, 0.5)',
+                    borderColor: 'rgba(78, 115, 223, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                responsive: true,
+                scales: {
+                    x: {
+                        grid: {
+                            display: false
+                        },
+                        ticks: {
+                            maxRotation: 0,
+                            autoSkip: true
+                        }
                     },
-                    ticks: {
-                        maxRotation: 0,
-                        autoSkip: true
+                    y: {
+                        beginAtZero: true,
+                        ticks: {
+                            callback: function (value, index, values) {
+                                if (Math.floor(value) === value) {
+                                    return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                }
+                            }
+                        }
                     }
                 },
-                y: {
-                    beginAtZero: true,
-                    ticks: {
-                        callback: function (value, index, values) {
-                            if (Math.floor(value) === value) {
-                                return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                plugins: {
+                    tooltip: {
+                        callbacks: {
+                            label: function (context) {
+                                var value = context.parsed.y;
+                                if (Math.floor(value) === value) {
+                                    return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                                } else {
+                                    return '₱' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+                                }
                             }
                         }
                     }
+                },
+                legend: {
+                    display: false
                 }
-            },
-            plugins: {
-                tooltip: {
-                    callbacks: {
-                        label: function (context) {
-                            var value = context.parsed.y;
-                            if (Math.floor(value) === value) {
-                                return '₱' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-                            } else {
-                                return '₱' + value.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-                            }
-                        }
-                    }
-                }
-            },
-            legend: {
-                display: false
             }
-        }
-    });
+        });
     </script>
 </body>
+
 </html>
